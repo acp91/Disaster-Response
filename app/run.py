888ebfe86e0caa321c274fe7bc907472a0efca22
Andrej <andrej.prek91@gmail.com>
2021-04-18 10:01:28 +0200

    includes train_classifier.py changes, and pickle file for my model

--- app/run.py
+++ app/run.py
@@ -8,7 +8,8 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+#from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 
@@ -30,7 +31,7 @@ engine = create_engine('sqlite:///../data/YourDatabaseName.db')
 df = pd.read_sql_table('YourTableName', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../modelscon/your_model_name.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
